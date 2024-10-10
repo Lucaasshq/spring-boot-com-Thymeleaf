800890f95335381f4ce6ac0f4c99f7b75303503d
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-10-10 09:46:17 -0300

    initial commit

---
+++ src/test/java/org/lucashspring3/curso/demomvc/curso/DemoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lucashspring3.curso.demomvc.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
