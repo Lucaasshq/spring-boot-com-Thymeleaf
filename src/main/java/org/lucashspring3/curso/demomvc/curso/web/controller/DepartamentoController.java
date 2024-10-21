9ae5ff3e20f0ec0b4b3dfef0023168eadb21ccc7
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-10-20 21:20:08 -0300

    implementando método salvar na pagina

--- src/main/java/org/lucashspring3/curso/demomvc/curso/web/controller/DepartamentoController.java
+++ src/main/java/org/lucashspring3/curso/demomvc/curso/web/controller/DepartamentoController.java
@@ -1,15 +1,24 @@
 package org.lucashspring3.curso.demomvc.curso.web.controller;
 
+import org.lucashspring3.curso.demomvc.curso.entity.Departamento;
+import org.lucashspring3.curso.demomvc.curso.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 
+    @Autowired
+    DepartamentoService departamentoService;
+
     @GetMapping("/cadastrar")
-    public String cadastro() {
+    public String cadastro(Model model) {
+        model.addAttribute("departamento", new Departamento());
      return "/departamento/cadastro";
     }
 
@@ -17,6 +26,12 @@ public class DepartamentoController {
     @GetMapping("/listar")
     public String listar() {
         return "/departamento/lista";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento) {
+        departamentoService.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
     }
 
 
