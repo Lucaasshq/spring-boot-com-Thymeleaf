2f5da8645a3e4472959b7b177f43a8f60d057e1c
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-11-05 01:55:10 -0300

    add funcao de listar departamentos do banco de dados

--- src/main/java/org/lucashspring3/curso/demomvc/curso/web/controller/DepartamentoController.java
+++ src/main/java/org/lucashspring3/curso/demomvc/curso/web/controller/DepartamentoController.java
@@ -3,11 +3,15 @@ package org.lucashspring3.curso.demomvc.curso.web.controller;
 import org.lucashspring3.curso.demomvc.curso.entity.Departamento;
 import org.lucashspring3.curso.demomvc.curso.service.DepartamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/departamentos")
@@ -17,15 +21,18 @@ public class DepartamentoController {
     DepartamentoService departamentoService;
 
     @GetMapping("/cadastrar")
-    public String cadastro(Model model) {
+    public String cadastro(@ModelAttribute Departamento departamento, Model model) {
         model.addAttribute("departamento", new Departamento());
+//        departamentoService.salvar(departamento);
      return "/departamento/cadastro";
         
     }
 
 
     @GetMapping("/listar")
-    public String listar() {
+    public String listar(Model model) {
+        List<Departamento> departamentos = departamentoService.buscarTodosDepartamentos(); // Certifique-se de que este método retorna a lista correta
+        model.addAttribute("departamentos", departamentos);
         return "/departamento/lista";
     }
 
